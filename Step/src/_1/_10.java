f03c77df2432aa734744d2a3b2791cdeaf24f9de
kimYunsoo <kimdia200@naver.com>
2019-07-03 00:22:04 +0900

    1단계 완료

---
+++ Step/src/_1/_10.java
@@ -0,0 +1,19 @@
+package _1;
+//√‚√≥ : https://www.acmicpc.net/problem/10869
+import java.util.Scanner;
+
+public class _10 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a,b;
+		a=input.nextInt();
+		b=input.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
