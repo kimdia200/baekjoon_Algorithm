96c6c83c6a0ce1ab5eb4350b0c5da06e6d83ffa1
kim yun su <kimdia2000@gmail.com>
2021-01-01 23:03:14 +0900

    chepter4_1

---
+++ Step/src/_5_Array/_1.java
@@ -0,0 +1,31 @@
+package _5_Array;
+
+import java.util.Scanner;
+
+/**
+ * URL: https://www.acmicpc.net/problem/10818
+ * 
+ */
+public class _1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[sc.nextInt()];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		int min=arr[0];
+		int max=arr[0];
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]>max)
+				max=arr[i];
+			if(arr[i]<min)
+				min=arr[i];
+		}
+		
+		System.out.println("min = "+min+", max = "+max);
+	}
+}
