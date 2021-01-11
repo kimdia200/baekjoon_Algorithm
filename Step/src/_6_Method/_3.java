8b62d2f8bab9c9d2c21da3731476b97dde43e499
kimyunsu <kimdia2000@gmail.com>
2021-01-12 01:47:57 +0900

    chepter7. String

---
+++ Step/src/_6_Method/_3.java
@@ -0,0 +1,34 @@
+package _6_Method;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/1065
+ * 
+ */
+public class _3 {
+	public static void main(String[] args) {
+		_3 t = new _3();
+		Scanner sc = new Scanner(System.in);
+		System.out.println(t.hansu(sc.nextInt()));
+	}
+	
+	public int hansu(int num) {
+		int count=0;
+		
+		for (int i = 1; i <= num; i++) {
+			if(i<100) {
+				count++;
+				continue;
+			}
+			int num1 = i/100;
+			int num2 = i/10%10;
+			int num3 = i%10;
+			
+			if((num1-num2)==(num2-num3)) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
---
+++ Step/src/_7_String/_4.java
@@ -0,0 +1,24 @@
+package _7_String;
+
+import java.util.Scanner;
+
+/**
+ * URL:https://www.acmicpc.net/problem/2675
+ *
+ */
+public class _4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		String s = sc.nextLine();
+		
+		for(int i=0; i<s.length();i++) {
+			for (int j = 0; j < num; j++) {
+				System.out.print(s.charAt(i));
+			}
+		}
+		
+	}
+}
