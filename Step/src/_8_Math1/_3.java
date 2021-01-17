fc79e3ba620abd7e5cc6e3fb643978b297d64ff7
kimyunsu <kimdia2000@gmail.com>
2021-01-17 23:06:34 +0900

    Test

---
+++ Step/src/_8_Math1/_3.java
@@ -0,0 +1,12 @@
+package _8_Math1;
+/**
+ * URL: https://www.acmicpc.net/problem/1193
+ *
+ */
+public class _3 {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
---
+++ Step/src/_8_Math1/_4.java
@@ -0,0 +1,26 @@
+package _8_Math1;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/2869
+ *
+ */
+public class _4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		
+		int day = 1;
+		
+		while(true) {
+			
+			
+		}
+		
+	}
+}
