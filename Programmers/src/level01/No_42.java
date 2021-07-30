c5695fe36e25a00ecd0d96be74cc126ea6f26b80
kimyunsu <kimdia2000@gmail.com>
2021-07-30 22:08:18 +0900

    [프로그래머스] 연습문제 level1 끝

---
+++ Programmers/src/level01/No_42.java
@@ -0,0 +1,26 @@
+package level01;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12948
+ *
+ * 연습문제 핸드폰 번호 가리기
+ * 
+ * 두번째 코드가 훨씬 시간복잡도가 적다
+ */
+public class No_42 {
+	public static void main(String[] args) {
+		System.out.println(new No_42().solution("01033334444"));
+	}
+	
+	public String solution(String s) {
+		return s.substring(0,s.length()-4).replaceAll("[0-9]", "*")+s.substring(s.length()-4);
+    }
+	
+	public String solution2(String s) {
+		char[] ch = s.toCharArray();
+	    for(int i = 0; i < ch.length - 4; i ++){
+	    	ch[i] = '*';
+	    }
+	    return String.valueOf(ch);
+	}
+}
---
+++ Programmers/src/level01/No_45.java
@@ -0,0 +1,83 @@
+package level01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12940
+ *
+ * 연습문제 - 최대공약수와 최소공배수
+ */
+public class No_45 {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(new No_45().solution(3,12)));
+	}
+	
+	// 다른사람이 푼것 대박....
+	public int[] gcdlcm(int a, int b) {
+		int[] answer = new int[2];
+
+		answer[0] = gcd(a, b);
+		answer[1] = (a * b) / answer[0];
+		return answer;
+	}
+
+	public static int gcd(int p, int q) {
+		if (q == 0)
+			return p;
+		return gcd(q, p % q);
+	}
+	
+	
+	//내가푼거
+	public int[] solution(int n, int m) {
+		if(n==m) {
+			return new int[] {n,n};
+		}
+		List<Integer> list1 = new ArrayList<Integer>();
+		List<Integer> list2 = new ArrayList<Integer>();
+		
+		for(int i=1; i<=n/2; i++) {
+			if(n%i == 0) {
+				list1.add(i);
+				list1.add(n/i);
+			}
+		}
+		for(int i=1; i<=m/2; i++) {
+			if(m%i == 0) {
+				list2.add(i);
+				list2.add(m/i);
+			}
+		}
+		
+		list1.sort(null);
+		
+		int gcd=1;
+		for(int x : list1) {
+			if(list2.contains(x)) {
+				gcd=x;
+			}
+		}
+		
+		int small;
+		int big;
+		if(n<m) {
+			small=n;
+			big=m;
+		}else {
+			small=m;
+			big=n;
+		}
+		
+		int lcm=big;
+		for(int i=2;;i++) {
+			if(lcm%small==0) {
+				break;
+			}else {
+				lcm=big*i;
+			}
+		}
+		return new int[] {gcd, lcm};
+    }
+}
