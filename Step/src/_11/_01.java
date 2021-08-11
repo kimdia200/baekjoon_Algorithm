98a699d9f2cf6d6cb45d96d8f551c4a979201ebf
kimyunsu <kimdia2000@gmail.com>
2021-08-11 23:38:49 +0900

    [백준] step-11 블랙잭

---
+++ Step/src/_11/_01.java
@@ -0,0 +1,56 @@
+package _11;
+
+import java.util.Scanner;
+
+public class _01 {
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int M = in.nextInt();
+ 
+		int[] arr = new int[N];
+ 
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		int result = search(arr, N, M);
+		System.out.println(result);
+	}
+ 
+	
+	// 탐색
+	static int search(int[] arr, int N, int M) {
+		int result = 0;
+ 
+		// 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
+		for (int i = 0; i < N - 2; i++) {
+ 
+			// 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
+			for (int j = i + 1; j < N - 1; j++) {
+ 
+				// 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
+				for (int k = j + 1; k < N; k++) {
+					
+					// 3개 카드의 합 변수 temp
+					int temp = arr[i] + arr[j] + arr[k];
+					
+					// M과 세 카드의 합이 같다면 temp return 및 종료 
+					if (M == temp) {	
+						return temp;
+					}
+					
+					// 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신 
+					if(result < temp && temp < M) {
+						result = temp;
+					}
+				}
+			}
+		}
+		
+		// 모든 순회를 마치면 result 리턴 
+		return result;
+	}
+}
